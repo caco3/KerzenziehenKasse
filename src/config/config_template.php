6e51dc3282f4b2b983e251f70a25d07bd0cdd08b
CaCO3x <caco3@ruinelli.ch>
2018-02-04 16:43:23 +0100

    added config template

---
+++ src/config/config_template.php
@@ -0,0 +1,18 @@
+<?
+error_reporting(~E_ALL);
+error_reporting(E_ALL); // Enable showing all error
+ 
+ 
+define ( 'MYSQL_USER',      '***' );
+define ( 'MYSQL_PASSWORD',  '***' );
+  
+ 
+define ( 'MYSQL_HOST',      '***' );
+define ( 'MYSQL_DATABASE',  '***' );
+
+
+define ( 'PASSWORD_HASH',   '***' );
+define ( 'ID',              '***' );
+
+
+?>
