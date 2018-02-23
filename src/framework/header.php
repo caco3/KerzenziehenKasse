f036fd9e4273b94d172d1c7ff187d0123cb117d5
caco3 <caco3@ruineli.ch>
2018-02-23 17:18:34 +0100

    Added more pages

--- src/framework/header.php
+++ src/framework/header.php
@@ -52,14 +52,21 @@ db_connect();
             <div id=headerLinksDiv>
                 <p id=headerLinksTexts>
                 <? if(basename($_SERVER['PHP_SELF']) == "index.php") { ?>
-                    <a id=receiptTrigger class="headerLinks" href="receipt.php" target="_self" 
-                        onclick="firework.launch('Erstelle Beleg...', 'success', 5000);"
-                        >Beleg zu letzter Buchung</a> | 
-                    <a class="headerLinks" href="stats.php" target="_blank">Auswertung</a>
+                    <a id=receiptTrigger class="headerLinks" href="receipt.php" target="_self" onclick="firework.launch('Erstelle Beleg...', 'success', 5000);" >Beleg zu letzter Buchung</a>
+                    | <a class="headerLinks" href="bookings.php" target="_self">Buchungen</a>
+                    | <a class="headerLinks" href="stats.php" target="_self">Auswertung</a>
+                    | <a class="headerLinks" href="admin.php" target="_self">Administration</a>
+                    | <a class="headerLinks" href="help.php" target="_blank">Hilfe</a>
+                <? } 
+                    elseif(basename($_SERVER['PHP_SELF']) == "bookings.php") { ?>
+                    <a class="headerLinks" href="index.php" target="_self">Hauptseite</a>
+                    | <a class="headerLinks" href="stats.php" target="_self">Auswertung</a>
+                    | <a class="headerLinks" href="admin.php" target="_self">Administration</a>
                     | <a class="headerLinks" href="help.php" target="_blank">Hilfe</a>
                 <? } 
                     elseif(basename($_SERVER['PHP_SELF']) == "stats.php") { ?>
-                    <a class="headerLinks" href="index.php" target="_blank">Hauptseite</a>
+                    <a class="headerLinks" href="index.php" target="_self">Hauptseite</a>
+                    | <a class="headerLinks" href="admin.php" target="_self">Administration</a>
                     | <a class="headerLinks" href="help.php" target="_blank">Hilfe</a>
                 <? } 
                     elseif(basename($_SERVER['PHP_SELF']) == "help.php") { ?>
--- src/help.php
+++ src/help.php
@@ -6,7 +6,10 @@ include "$root/framework/header.php";
       <h1>Hilfe</h1>
       <h2>FAQ</h2>
       <ul>
-        <li><p>Frage: Wohin kann ich Feedback und Verbesserungsvorschläge senden?<br>Antwort: Siehe Support unten.</p></li>
+        <li><p class=question><b>Frage:</b> Wohin kann ich Feedback und Verbesserungsvorschläge senden?</p>
+            <p class=answer><b>Antwort:</b> Siehe Support unten.</p></li>
+        <li><p class=question><b>Frage:</b> Wieso zeigt Word eine Fehlermeldung, wenn ich den Beleg öffne?</p>
+            <p class=answer><b>Antwort:</b> Word unterstützt leider die aktuelle Version des Opendocument Formats (*.odt) nicht vollständig. Die Fehlermeldung kann jedoch ignoriert werden.<br>Alternativ kann <a href=https://de.libreoffice.org/ target="_blank">Libreoffice</a> (gratis)</p></li>
       </ul>
       <h2>Support</h2>
       <p>George Ruinelli<br>
