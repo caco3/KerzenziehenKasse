e75de3e1753188470204fabee67e63ef02c126bf
chrischona-uster.ch <chrisc22@s27.server.hostpoint.ch>
2018-10-14 23:29:14 +0200

    updated stats placeholder

--- src/stats.php
+++ src/stats.php
@@ -3,10 +3,18 @@ $root=".";
 include "$root/framework/header.php";
 ?>
     <div id="body">
-      <h1>Auswertung (Aktuelles Jahr)</h1>
+      <h1>Auswertung (Aktueller Tag)</h1>
+      <h2>Nach Kosten</h2>
+      <p>Noch nicht implementiert</p>
       <h2>Nach Menge</h2>
       <p>Noch nicht implementiert</p>
+      
+      
+      
+      <h1>Auswertung (Aktuelles Jahr)</h1>
       <h2>Nach Kosten</h2>
+      <p>Noch nicht implementiert</p>
+      <h2>Nach Menge</h2>
       <p>Noch nicht implementiert</p>
     </div>
 
