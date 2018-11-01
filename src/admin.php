f44f0fd8c4c7eca86d916ec1ba540fd581389b5e
chrischona-uster.ch <chrisc22@s27.server.hostpoint.ch>
2018-11-01 21:32:39 +0100

    updated admin page

--- src/admin.php
+++ src/admin.php
@@ -5,13 +5,13 @@ include "$root/framework/header.php";
     <div id="body">
       <h1>Administration</h1>
       <h2>Artikel und Preise ändern</h2>
-      <p><a href=https://admin.hostpoint.ch/phpmyadmin2/index.php?lang=de&server=mysql19j06.db.hostpoint.internal target=_blank">phpMyAdmin</a> (Zugangsdaten bei George Ruinelli oder Michael Zollinger anfordern).</p>
+      <p><a href=https://admin.hostpoint.ch/phpmyadmin2/index.php?lang=de&server=mysql19j06.db.hostpoint.internal target="_blank">phpMyAdmin</a> (Zugangsdaten bei George Ruinelli oder Michael Zollinger anfordern).</p>
       
       <h2>Buchungen exportieren</h2>
-      <p>Noch nicht implementiert</p>
+      <p>Siehe <a href=stats.php target="_self">Statistik-Seite</a>.</p>
       
       <h2>Datenbank exportieren</h2>
-      <p><a href=subpages/exportDb.php target=_blank">Export im SQL-Format</a>.</p>
+      <p><a href=subpages/exportDb.php target="_self">Export im SQL-Format</a>.</p>
       
     </div>
 
