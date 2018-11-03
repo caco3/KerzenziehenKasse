fe68fbf6a4314b72740a4430199bf4eab72406be
chrischona-uster.ch <chrisc22@s27.server.hostpoint.ch>
2018-11-03 21:54:36 +0100

    adjusted line breaks

--- src/admin.php
+++ src/admin.php
@@ -5,13 +5,13 @@ include "$root/framework/header.php";
     <div id="body">
       <h1>Administration</h1>
       <h2>Artikel und Preise ändern</h2>
-      <p><a href=https://admin.hostpoint.ch/phpmyadmin2/index.php?lang=de&server=mysql19j06.db.hostpoint.internal target="_blank">phpMyAdmin</a> (Zugangsdaten bei George Ruinelli oder Michael Zollinger anfordern).</p>
+      <p><a href=https://admin.hostpoint.ch/phpmyadmin2/index.php?lang=de&server=mysql19j06.db.hostpoint.internal target="_blank">phpMyAdmin</a> (Zugangsdaten bei George Ruinelli oder Michael Zollinger anfordern).<br><br></p>
       
       <h2>Buchungen exportieren</h2>
-      <p>Siehe <a href=stats.php target="_self">Statistik-Seite</a>.</p>
+      <p>Siehe <a href=stats.php target="_self">Statistik-Seite</a>.<br><br></p>
       
       <h2>Datenbank exportieren</h2>
-      <p><a href=subpages/exportDb.php target="_self">Export im SQL-Format</a>.</p>
+      <p><a href=subpages/exportDb.php target="_self">Export im SQL-Format</a>.<br><br></p>
       
     </div>
 
--- src/help.php
+++ src/help.php
@@ -8,32 +8,32 @@ include "$root/framework/header.php";
       <ul>
         
         <li><p class=question><b>Frage:</b> Was bedeutet die kleine Stopuhr <img src=images/timer/5.png> oben in der Navigationsbar?</p>
-            <p class=answer><b>Antwort:</b> Wenn im Warenkorb in einem Testfeld eine Eingabe gemacht wird (Menge- oder Preisänderung), wird einen Moment gewartet, bevor der Warenkorb neu berechnet wird.</p></li>
+            <p class=answer><b>Antwort:</b> Wenn im Warenkorb in einem Testfeld eine Eingabe gemacht wird (Menge- oder Preisänderung), wird einen Moment gewartet, bevor der Warenkorb neu berechnet wird.<br><br></p></li>
                 
         <li><p class=question><b>Frage:</b> Wieso kann man nur die Buchungen vom aktuellen Tag nachbearbeiten?</p>
-            <p class=answer><b>Antwort:</b> Die Tageskasse von den vergangenen Tagen wurden bereits abgerechnet ünd können nicht mehr verändert werden.</p></li>
+            <p class=answer><b>Antwort:</b> Die Tageskasse von den vergangenen Tagen wurden bereits abgerechnet und können nicht mehr verändert werden.<br><br></p></li>
         
         <li><p class=question><b>Frage:</b> Wohin kann ich Feedback und Verbesserungsvorschläge senden?</p>
-            <p class=answer><b>Antwort:</b> Siehe Support unten.</p></li>
+            <p class=answer><b>Antwort:</b> Siehe Support unten.<br><br></p></li>
         
         <li><p class=question><b>Frage:</b> Wieso zeigt Word eine Fehlermeldung, wenn ich den Beleg öffne?</p>
-            <p class=answer><b>Antwort:</b> Word unterstützt leider die aktuelle Version des Opendocument Formats (*.odt) nicht vollständig. Die Fehlermeldung kann jedoch ignoriert werden.<br>Alternativ kann <a href=https://de.libreoffice.org/ target="_blank">Libreoffice</a> verwendet werden (Gratis Alternative für Microsoft Office).</p></li>
+            <p class=answer><b>Antwort:</b> Word unterstützt leider die aktuelle Version des Opendocument Formats (*.odt) nicht vollständig. Die Fehlermeldung kann jedoch ignoriert werden.<br>Alternativ kann <a href=https://de.libreoffice.org/ target="_blank">Libreoffice</a> verwendet werden (Gratis Alternative für Microsoft Office).<br><br></p></li>
             
         <li><p class=question><b>Frage:</b> Wie können die Preise angepasst werden?</p>
-            <p class=answer><b>Antwort:</b> Die Artikel und Preise können über <a href="https://admin.hostpoint.ch/phpmyadmin2/sql.php?db=chrisc22_kerzenziehen&table=articles&server=467&pos=0" target="_blank">phpMyAdmin</a> angepasst werden.</p></li>
+            <p class=answer><b>Antwort:</b> Die Artikel und Preise können über <a href="https://admin.hostpoint.ch/phpmyadmin2/sql.php?db=chrisc22_kerzenziehen&table=articles&server=467&pos=0" target="_blank">phpMyAdmin</a> angepasst werden.<br><br></p></li>
             
         <li><p class=question><b>Frage:</b> Gibt es einen Backup?</p>
-            <p class=answer><b>Antwort:</b> Es wird täglich ein Backup der Datenbank gemacht. Die Daten werden abgelegt unter ~/backup/kerzenziehen.</p></li>
+            <p class=answer><b>Antwort:</b> Es wird täglich ein Backup der Datenbank gemacht. Die Daten werden abgelegt unter ~/backup/kerzenziehen.<br><br></p></li>
             
         <li><p class=question><b>Frage:</b> Wo finde ich den Quellcode dieser Buchungssoftware?</p>
-            <p class=answer><b>Antwort:</b> Siehe <a href="https://github.com/caco3/KerzenziehenKasse" target="_blank">https://github.com/caco3/KerzenziehenKasse</a>.</p></li>
-      
+            <p class=answer><b>Antwort:</b> Siehe <a href="https://github.com/caco3/KerzenziehenKasse" target="_blank">https://github.com/caco3/KerzenziehenKasse</a>.<br><br></p></li>
       </ul>
+      
       <h2>Support</h2>
       <p>George Ruinelli<br>
         <a href="mailto:george@ruinelli.ch">george@ruinelli.ch</a><br>
         Mobile: 076/418'22'61
-      </p>
+      <br><br></p>
       <h2>Copyright</h2>
       <p>&copy; 2018 by George Ruinelli</p>
     </div>
