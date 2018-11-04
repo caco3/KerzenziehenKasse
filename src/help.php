26c75723ba3417c6851181bfdab36a80ac9db34a
chrischona-uster.ch <chrisc22@s27.server.hostpoint.ch>
2018-11-04 14:05:49 +0100

    updated help

--- src/help.php
+++ src/help.php
@@ -34,6 +34,9 @@ include "$root/framework/header.php";
         <a href="mailto:george@ruinelli.ch">george@ruinelli.ch</a><br>
         Mobile: 076/418'22'61
       <br><br></p>
+      <p>Philipp Huber<br>
+        Mobile: 079/471'63'90
+      <br><br></p>
       <h2>Copyright</h2>
       <p>&copy; 2018 by George Ruinelli</p>
     </div>
