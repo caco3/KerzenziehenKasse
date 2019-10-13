aa6156c6930c52e1917a9519386f6d783d8ae5e6
chrischona-uster.ch <chrisc22@s27.server.hostpoint.ch>
2019-10-13 22:16:58 +0200

    adjusted navigation style

--- src/subpages/navigation.php
+++ src/subpages/navigation.php
@@ -1,4 +1,4 @@
-<p><a class="headerLinks" href="bookings.php" target="_self">Buchungen</a></p>
-<p><a class="headerLinks" href="stats.php" target="_self">Auswertung</a></p>
-<p><a class="headerLinks" href="admin.php" target="_self">Administration</a></p>
-<p><a class="headerLinks" href="help.php" target="_self">Hilfe</a></p>
+<p class=navigation><a class="headerLinks" href="bookings.php" target="_self">Buchungen</a></p>
+<p class=navigation><a class="headerLinks" href="stats.php" target="_self">Auswertung</a></p>
+<p class=navigation><a class="headerLinks" href="admin.php" target="_self">Administration</a></p>
+<p class=navigation><a class="headerLinks" href="help.php" target="_self">Hilfe</a></p>
