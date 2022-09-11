07642880d55386e34f138e501db497864bbd6573
CaCO3 <caco@ruinelli.ch>
2022-09-11 21:47:18 +0200

    .

---
+++ src/framework/qr_bill.php
@@ -0,0 +1,58 @@
+<?php
+
+$root=".";
+define('DIR_CLASSES', "$root/framework/qrBill/");
+
+
+use Sprain\SwissQrBill as QrBill;
+
+require $root . '/framework/qrBill/vendor/autoload.php';
+
+
+function generateQrCode($bookingId) {
+    global $root;
+
+    $qrBill = QrBill\QrBill::create();
+
+    // Add creditor information
+    // Who will receive the payment and to which bank account?
+    $qrBill->setCreditor(
+        QrBill\DataGroup\Element\CombinedAddress::create(
+            'Chrischona Schweiz',
+            'Chrischona-Gemeinde Uster',
+            '4126 Bettingen',
+            'CH'
+        ));
+
+    $qrBill->setCreditorInformation(
+        QrBill\DataGroup\Element\CreditorInformation::create(
+            'CH0506888016123250007' // This is a classic iban. QR-IBANs will not be valid in this minmal setup.
+        ));
+
+    // Add payment amount information
+    // The currency must be defined.
+    $qrBill->setPaymentAmountInformation(
+        QrBill\DataGroup\Element\PaymentAmountInformation::create(
+            'CHF'
+        ));
+
+    // Add payment reference
+    // Explicitly define that no reference number will be used by setting TYPE_NON.
+    $qrBill->setPaymentReference(
+        QrBill\DataGroup\Element\PaymentReference::create(
+            QrBill\DataGroup\Element\PaymentReference::TYPE_NON
+        ));
+
+
+    // Optionally, add some human-readable information about what the bill is for.
+    $qrBill->setAdditionalInformation(
+        QrBill\DataGroup\Element\AdditionalInformation::create(
+            "Kerzenziehen $bookingId")
+    );
+
+
+    $qrBill->getQrCode()->writeFile($root . "/../tmp/qrBill_$bookingId.png");
+
+}
+
+?>
---
+++ src/testQrBill.php
@@ -0,0 +1,10 @@
+<?php
+
+
+require_once("framework/qr_bill.php");
+
+$bookingId = 9988;
+
+generateQrCode($bookingId);
+
+?>
