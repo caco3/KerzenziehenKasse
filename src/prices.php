379fc8ec4998785688d66b5d653ee7f1dbc3d0a3
caco3 <caco3@ruinelli.ch>
2022-10-01 16:08:32 +0200

    improved preisliste formating

--- src/prices.php
+++ src/prices.php
@@ -41,21 +41,40 @@ $articles = array_merge($articles, getDbProductsEx("wachs", "name", "effect"));
 
 ?>
 
-<h1>Preislisten-Generator (Stand <? echo(date("d.m.Y")); ?>)</h1>
+<div id="scoped-content">
+    <style type="text/css" scoped>
+        table, th, td { border:1px solid black; border-collapse: collapse; padding: 5px;  } 		
+    </style>
+
+<h1>Preisliste (Stand <? echo(date("d.m.Y")); ?>)</h1>
 <p>Alles auswählen und in den Texteditor deiner Wahl einfügen.</p>
-
+	
 <table>
-
+<tr><th colspan=2>Artikel</th><th>Preis</th><th>Ungefähre Dauer</th></tr>
 <?
 foreach($articles as $article) {
-    if ($article['typ'] == "wachs") {
+    if (($article['typ'] == "wachs") && ($article['subtype'] != "effect")) { // Ziehen (ohne Effektwachs)
         $article['pricePerQuantity'] = $article['pricePerQuantity'] * 100; // Adjust price since it is per 'g' but we want to show it per '100g'
+		$duration = $article['duration'] . " h für &Oslash; 1 - 1.5 cm";
     }
+	elseif (($article['typ'] == "guss") && ($article['subtype'] != "preMade")) { // Giessen (ohne Rosenkerze)
+		$duration = $article['duration'] . " h";
+    }
+	else {
+		$duration = "-";
+	}
+	
+	
+	
+	
     ?>
     <tr>
         <td><img src=images/articles/<? echo($article['image1']); ?> style="width: 50px; height: 50px;"></td>
         <td><? echo($article['name']); ?></td>
         <td>CHF <? echo(formatMoney($article['pricePerQuantity'])); ?>/<? echo($article['package']); ?></td>
+        <td><? echo($duration); ?></td>
+		
+		
     </tr>
     <?
 }
@@ -63,3 +82,4 @@ foreach($articles as $article) {
 ?>
 
 </table>
+</div>
