535baccf579c5fff5cfd526434730b2e98cd0905
CaCO3 <caco3@ruinelli.ch>
2026-01-06 21:30:50 +0100

    .

--- src/ajax/getBookingData.php
+++ src/ajax/getBookingData.php
@@ -9,14 +9,6 @@ require_once("$root/framework/db.php");
 db_connect();
 
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-
-// Handle preflight OPTIONS request
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    exit(0);
-}
 
 // Get booking ID from request
 if(isset($_GET['id'])) {
