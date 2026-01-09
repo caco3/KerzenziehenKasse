a0edfb8d475aef1c49c8428fa41e76f520e2d7ce
CaCO3 <caco3@ruinelli.ch>
2026-01-09 18:01:24 +0100

    .

--- src/statsDiagrams2.php
+++ src/statsDiagrams2.php
@@ -18,24 +18,7 @@ $totalWaxPerDayAndYearInKgSummed = $statsData['totalWaxPerDayAndYearInKgSummed']
 ?>
 
 <script>
-var data = [
-    ['', '2017: Öffentlich', '2017: Schule/Geschlossene Gesellschaft/Private Gruppe', '2018: Öffentlich', '2018: Schule/Geschlossene Gesellschaft/Private Gruppe', '2019: Öffentlich', '2019: Schule/Geschlossene Gesellschaft/Private Gruppe', '2021: Öffentlich', '2021: Schule/Geschlossene Gesellschaft/Private Gruppe', '2022: Öffentlich', '2022: Schule/Geschlossene Gesellschaft/Private Gruppe', '2023: Öffentlich', '2023: Schule/Geschlossene Gesellschaft/Private Gruppe', '2024: Öffentlich', '2024: Schule/Geschlossene Gesellschaft/Private Gruppe', '2025: Öffentlich', '2025: Schule/Geschlossene Gesellschaft/Private Gruppe', '2026: Öffentlich', '2026: Schule/Geschlossene Gesellschaft/Private Gruppe'],
-    ['So', 900.0, 400.0, 2000.0, 400.0, 1920.3, 500.0, 1382.5, 500.0, 1914.2, 500.0, 2026.5, 0.0, 1568.4, 0.0, 1630.2, 0.0, 2009.0, 1700.5],
-    ['Mo', 100, 0, 0.0, 100.7, 0, 0, 113.9, 100.0, 0.0, 317.2, 0.0, 150.0, 5.0, 452.8, 0.0, 150.0, 50.5, 1072.0],
-    ['Di', 1000.0, 100.0, 0, 0, 0.0, 148.2, 48.1, 125.5, 0.0, 150.0, 0.0, 300.0, 0.0, 300.0, 54.0, 420.1, 26.5, 57.5],
-    ['Mi', 873.8, 0.0, 777.3, 0.0, 769.1, 100.0, 826.3, 120.5, 915.5, 0.0, 1039.0, 150.0, 931.7, 150.0, 1297.4, 150.0, 23.0, 1061.5],
-    ['Do', 1000.0, 217.0, 0.0, 135.8, 86.1, 0.0, 0.0, 106.3, 0.0, 300.0, 3.7, 316.3, 0.0, 333.3, 0.0, 336.7, 0, 1000],
-    ['Fr', 487.0, 0.0, 322.8, 0.0, 882.1, 217.3, 508.4, 332.1, 986.6, 782.3, 572.6, 150.0, 1048.5, 185.8, 945.0, 300.0, 0, 1000],
-    ['Sa', 1086.6, 0.0, 1140.5, 0.0, 1718.2, 0.0, 1886.3, 0.0, 2732.2, 0.0, 1909.0, 0.0, 1938.8, 0.0, 2058.3, 0.0, 0, 1000],
-    ['So', 1560.6, 0.0, 1517.9, 0.0, 3058.7, 0.0, 2507.2, 0.0, 3350.8, 0.0, 2883.3, 0.0, 3514.9, 0.0, 2927.6, 0.0, 0, 1000],
-    ['Mo', 1000, 0, 8.9, 226.5, 0, 0, 126.1, 274.0, 0.0, 322.5, 0.0, 300.0, 0.0, 470.0, 64.5, 360.5, 0, 1000],
-    ['Di', 1000.0, 142.5, 0.0, 125.3, 0, 0, 0.0, 203.0, 11.0, 300.0, 0.0, 300.0, 0.0, 301.7, 0.0, 307.5, 0, 1000],
-    ['Mi', 916.4, 0.0, 896.0, 581.7, 1239.3, 0.0, 1218.7, 150.3, 1556.9, 150.0, 1129.5, 150.0, 1721.1, 218.6, 1498.1, 150.0, 0, 1000],
-    ['Do', 241.5, 0.0, 0.0, 158.6, 0, 0, 2.0, 243.6, 0.0, 300.0, 127.0, 200.0, 0.0, 379.6, 3.0, 300.0, 0, 1000],
-    ['Fr', 765.5, 0.0, 883.2, 0.0, 883.1, 0.0, 1317.9, 229.7, 1705.4, 406.9, 1186.3, 371.5, 1424.6, 150.0, 1388.8, 250.0, 3000, 1000],
-    ['Sa', 1394.6, 0.0, 1852.3, 0.0, 2313.1, 0.0, 2583.8, 0.0, 3871.1, 0.0, 2830.4, 0.0, 3092.0, 0.0, 2848.6, 0.0, 2000, 1000],
-    ['So', 1275.8, 0.0, 1594.3, 0.0, 1909.8, 105.0, 1724.3, 0.0, 2604.4, 0.0, 2751.9, 0.0, 2729.6, 0.0, 3179.1, 0.0, 1000, 500]
-];
+var data = [];
 
 
 
@@ -140,13 +123,25 @@ for (var yearIndex = 0; yearIndex < 10; yearIndex++) {
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script>
     // Extract headers for series names
-    var headers = data[0].slice(1);
+    var headers = [];
     
     google.charts.load('current', {'packages':['corechart', 'bar']});
-    google.charts.setOnLoadCallback(function() {
-        drawChart();
-        drawLegendChart();
-    });
+
+    // Load chart data via AJAX
+    fetch('getChartData.php')
+        .then(response => response.json())
+        .then(chartData => {
+            data = chartData;
+            headers = data[0].slice(1);
+            google.charts.setOnLoadCallback(function() {
+                drawChart();
+                drawLegendChart();
+            });
+        })
+        .catch(error => {
+            console.error('Error loading chart data:', error);
+            firework.launch('Fehler beim Laden der Chart-Daten: ' + error, 'error', 5000);
+        });
 
     function drawChart() {
         // Prepare data for Google Charts - each year with 2 stacked values
